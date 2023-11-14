2f8f2ff0bc98277b390fe022900c9645770b9b76
SIBI RAJA <sraja@college.harvard.edu>
2023-11-14 02:01:43 -0500

    added cache for children

--- run.py
+++ run.py
@@ -9,6 +9,8 @@ from prompts import prompt, expansion_count, min_lines, check_fun
 
 montecarlo = MonteCarlo(Node(prompt))
 
+child_cache = {}
+
 def generate_complete(text, montecarlo):
     text = llm.generate(text, 1)[0]
     score = score_func(text)
@@ -27,14 +29,25 @@ def child_finder(node, montecarlo):
     if text is None:
         node.update_win_value(-1)
     else:
-        child = Node(text)
-        node.add_child(child)
-        child.update_win_value(1)
-        child.update_policy_value(1)
+        cached_node = child_cache.get(text)
+        if cached_node:
+            current_node = cached_node
 
-        child = Node(node.state)
-        node.add_child(child)
-        child.update_policy_value(0.2)
+            while current_node.expanded:
+                current_node = current_node.get_preferred_child(montecarlo.root_node)
+
+            montecarlo.expand(current_node)
+        else:
+            child = Node(text)
+            node.add_child(child)
+            child.update_win_value(1)
+            child.update_policy_value(1)
+
+            child_cache[text] = child
+
+            child = Node(node.state)
+            node.add_child(child)
+            child.update_policy_value(0.2)
 
 montecarlo.child_finder = child_finder
 
@@ -42,4 +55,3 @@ montecarlo.simulate(expansion_count)
 
 print('CHOSEN SOLUTION')
 print(montecarlo.solution)
-
